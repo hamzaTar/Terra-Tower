a631045cd5ce7c65598d26b191519f8c4d98c209
Lluís <lluismo@me.com>
2026-06-17 16:52:21 +0200

    Creacion de Player, saltar mal hecho (no tiene en cuenta el charging), no hay gravedad y caida enpicado (hay que revisar)

--- model/Player.py
+++ model/Player.py
@@ -1,6 +1,36 @@
 class Player:
-    def _init__(self, x, y, vx, vy):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.vx = vx
-        self.vy = vy
+        self.isOnGround = True
+        self.isCharging = False
+        self.vx = 4
+        self.vy = 6
+        # Puntuación y monedas
+        self.puntuacion = 0
+        self.monedas = 0
+        # Para habilidades
+        self.saltos_restantes = 1
+        self.habilidades = []
+
+    def movimiento(self, direccion):
+        if direccion == "izq":
+            self.x -= self.vx
+        elif direccion == "derch":
+            self.x += self.vx
+
+    def saltar(self):
+        for i in self.habilidades:
+            if i == "doubleJump":
+                self.saltos_restantes += 1
+        if self.isOnGround:
+            self.y -= self.vy
+            self.saltos_restantes -= 1
+            if not self.isOnGround and self.saltos_restantes > 0:
+
+    def gravedad(self):
+        pass
+
+    def caida_picado(self):
+        if not self.isOnGround:
+            self.y += self.vy
