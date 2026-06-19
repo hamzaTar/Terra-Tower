449df5c464c98b5b6c928d4bb3c28e8b602a82aa
Lluís <lluismo@me.com>
2026-06-19 23:45:36 +0200

    Clase Player finalizada, creacion de clase nivel y monedas

--- model/Player.py
+++ model/Player.py
@@ -2,6 +2,8 @@ class Player:
     def __init__(self, x, y, isOnCheckPoint):
         self.x = x
         self.y = y
+        self.player_w = 30
+        self.player_h = 30
         self.isOnGround = True
         self.isOnCheckPoint = isOnCheckPoint
         self.vx = 4
@@ -24,9 +26,17 @@ class Player:
 
     def saltar(self):
         if self.saltos_restantes > 0:
-            self.vy = -12
+            self.vy = -2 - 10*self.carga
             self.saltos_restantes -= 1
             self.isOnGround = False
+            self.carga = 0
+            self.isCharging = False
+
+    def cargar_salto(self):
+        self.isCharging = True
+        self.carga += 0.1
+        if self.carga > 1:
+            self.carga = 1
 
     def gravedad(self):
         self.vy += 0.8
@@ -38,22 +48,21 @@ class Player:
 
     def aterrizar(self):
         self.isOnGround = True
-        self.saltos_restantes = 1
+        self.saltos_restantes = self.saltos_maximos
         self.vy = 0
 
-
     def get_Bounds(self):
-        ancho = 800
-        alto = 500
-        ancho_paredes = 30
+        return (self.x, self.y, self.player_w, self.player_h)
 
     def desbloquear_habilidad(self, habilidad):
         self.habilidades[habilidad] = True
-        if self.habilidades["doubleJump"] == True:
-            self.saltos_maximos = 2
-        elif self.habilidades["tripleJump"] == True:
-            self.saltos_maximos = 3
-        elif self.habilidades["wallJump"] == True:
-            pass
-        elif self.habilidades["dash"] == True:
-            self.vx = 14
+        match habilidad:
+            case "doubleJump":
+                self.saltos_maximos = 2
+            case "tripleJump":
+                self.saltos_maximos = 3
+            case "wallJump":
+                self.vx = 2
+                self.vy = 5
+            case "dash":
+                self.vx = 14
