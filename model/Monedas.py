671602df4fdb3a30d6a3864e924e4b8e1ea9a6e2
Lluís <lluismo@me.com>
2026-06-20 08:51:29 +0200

    Clases monedas y nivel

--- model/Monedas.py
+++ model/Monedas.py
@@ -2,4 +2,12 @@ class Monedas:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.cogida = False
+        self.moneda_w = 5
+        self.moneda_h = 5
+        self.recogida = False
+
+    def get_Bounds(self):
+        return self.x, self.y, self.moneda_w, self.moneda_h
+
+    def recoger(self):
+        self.recogida = True
